c71a6b9c3f92f25b04bcc663fdc533cf616a7f28
Albert Alvarez Estelles <susokukapu@gmail.com>
2022-11-20 17:57:58 +0100

    feat: custom useResult

--- src/hooks/useResults.js
+++ src/hooks/useResults.js
@@ -1,6 +1,6 @@
 import { useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
-import { addvotationParticipating, getMyVotationsIds, gettingInfoVotationsFromUser, getallVotationsInfo, toggleCharging } from "../store"
+import { addvotationParticipating, getMyVotationsIds, gettingInfoVotationsFromUser, getallVotationsInfo, toggleCharging, startDeleteVotation } from "../store"
 
 
 export const useResults = () => {
@@ -8,26 +8,42 @@ export const useResults = () => {
     const dispatch = useDispatch()
     const { allVotationsInfo } = useSelector( state => state.result )
     const { user } = useSelector( state => state.auth )
-    const [ allVotationList, setAllVotationList ] = useState(allVotationsInfo)
+    const [ allVotationList, setAllVotationList ] = useState([])
+
+    const getFisrtRender = async() => {
+        dispatch( toggleCharging( true ) )
+
+        const { infoVotations } =  await dispatch( gettingInfoVotationsFromUser({ uid: user.uid }) )
+        setAllVotationList( infoVotations )
+
+        dispatch( toggleCharging( false ) )
+    }
 
     const getInfoVotationFromUser = async( ) => {
         dispatch( toggleCharging( true ) )
 
-        const votationParticipating =  await dispatch( gettingInfoVotationsFromUser({ uid: user.uid }) )
-        dispatch( getallVotationsInfo( votationParticipating.infoVotations ))
+        const { infoVotations } =  await dispatch( gettingInfoVotationsFromUser({ uid: user.uid }) )
+
+        dispatch( getallVotationsInfo( infoVotations ))
         
-        // const votationsIds = await dispatch( getMyVotationsIds({ uid: user.uid }))
-        // dispatch( addvotationParticipating( votationsIds ))
+        const votationsIds = await dispatch( getMyVotationsIds({ uid: user.uid }))
+        dispatch( addvotationParticipating( votationsIds ))
         
 
         dispatch( toggleCharging( false ) )
     }
 
+    const votationDelete = ( votationId, uidParticipants ) => {
+        dispatch( startDeleteVotation({ votationId, uidParticipants }))
+        setAllVotationList( allVotationList.filter( votation => votation._id !== votationId ))
+    }
 
     return {
         allVotationList,
         setAllVotationList,
 
-        getInfoVotationFromUser
+        getInfoVotationFromUser,
+        getFisrtRender,
+        votationDelete
     }
 }
